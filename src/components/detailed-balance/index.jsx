336d94759a2d23d5f40b46d6e70f7dc526581a6f
ASGladchenko <asgladchenko@gmail.com>
2024-03-07 17:27:32 +0200

    fixed empty state

--- src/components/detailed-balance/index.jsx
+++ src/components/detailed-balance/index.jsx
@@ -9,6 +9,8 @@ import { getCurrency } from '../../store/currency';
 
 export const DetailedBalance = ({ filledMonth }) => {
   const { usd, eur } = useSelector(getCurrency);
+  const isEmptyData = filledMonth.length === 0;
+  console.log(isEmptyData);
 
   return (
     <div className="w-full p-4 overflow-x-auto rounded-md shadow-lg scroll bg-zinc-300">
@@ -24,51 +26,59 @@ export const DetailedBalance = ({ filledMonth }) => {
         <Create width={32} height={32} />
       </Link>
 
-      <table className="w-full min-w-[1000px] text-center text-zinc-800 font">
-        <thead className="uppercase ">
-          <tr className="flex w-full border-t border-zinc-900">
-            <th className="w-full max-w-[80px] first:border-l border-r border-zinc-900">
-              №
-            </th>
-            <th className="w-full border-r first:border-l border-zinc-900">
-              Month
-            </th>
-            <th className="w-full border-r first:border-l border-zinc-900">
-              Revenues
-            </th>
-            <th className="w-full border-r first:border-l border-zinc-900">
-              Expenses
-            </th>
-            <th className="w-full border-r first:border-l border-zinc-900">
-              UAH balance
-            </th>
-            <th className="w-full border-r first:border-l border-zinc-900">
-              USD balance
-            </th>
-            <th className="w-full border-r first:border-l border-zinc-900">
-              EUR balance
-            </th>
-            <th className="w-full max-w-[80px] first:border-l border-r border-zinc-900">
-              Edit
-            </th>
-            <th className="w-full max-w-[80px] first:border-l border-r border-zinc-900">
-              Delete
-            </th>
-          </tr>
-        </thead>
+      {isEmptyData && (
+        <h2 className="pb-10 text-3xl text-center font-kode text-zinc-700">
+          Your finances are not available
+        </h2>
+      )}
 
-        <tbody className="text-zinc-600">
-          {filledMonth?.map((month, idx) => (
-            <BalanceMonth
-              {...month}
-              usd={usd?.rate}
-              eur={eur?.rate}
-              number={idx + 1}
-              key={`balance-${idx}-${month.name}`}
-            />
-          ))}
-        </tbody>
-      </table>
+      {!isEmptyData && (
+        <table className="w-full min-w-[1000px] text-center text-zinc-800 font">
+          <thead className="uppercase ">
+            <tr className="flex w-full border-t border-zinc-900">
+              <th className="w-full max-w-[80px] first:border-l border-r border-zinc-900">
+                №
+              </th>
+              <th className="w-full border-r first:border-l border-zinc-900">
+                Month
+              </th>
+              <th className="w-full border-r first:border-l border-zinc-900">
+                Revenues
+              </th>
+              <th className="w-full border-r first:border-l border-zinc-900">
+                Expenses
+              </th>
+              <th className="w-full border-r first:border-l border-zinc-900">
+                UAH balance
+              </th>
+              <th className="w-full border-r first:border-l border-zinc-900">
+                USD balance
+              </th>
+              <th className="w-full border-r first:border-l border-zinc-900">
+                EUR balance
+              </th>
+              <th className="w-full max-w-[80px] first:border-l border-r border-zinc-900">
+                Edit
+              </th>
+              <th className="w-full max-w-[80px] first:border-l border-r border-zinc-900">
+                Delete
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="text-zinc-600">
+            {filledMonth?.map((month, idx) => (
+              <BalanceMonth
+                {...month}
+                usd={usd?.rate}
+                eur={eur?.rate}
+                number={idx + 1}
+                key={`balance-${idx}-${month.name}`}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
